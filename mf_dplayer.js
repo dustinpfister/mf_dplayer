8f93b8206fd41e81ab435321bae05b9abe786829
dustinpfister <dustin.pfister@gmail.com>
2017-09-13 10:02:25 -0400

    ANI.set

--- mf_dplayer.js
+++ mf_dplayer.js
@@ -29,6 +29,19 @@ var dp = (function () {
 
     };
 
+    // set the animation according to it's current frame
+    ANI.prototype.set = function () {
+
+        var s = this;
+
+        s.p = s.f / s.mf;
+        s.b = 1 - Math.abs(.5 - s.p) / .5;
+
+        load[s.key].ff.call(s);
+
+    };
+
+    // public api
     return {
 
         // current stack of animations
@@ -42,12 +55,13 @@ var dp = (function () {
 
             // update animations
             this.stack.forEach(function (ani) {
-
+                /*
                 ani.p = ani.f / ani.mf;
                 ani.b = 1 - Math.abs(.5 - ani.p) / .5;
 
                 load[ani.key].ff.call(ani);
-
+                 */
+                ani.set();
                 ani.f++;
 
             });
@@ -79,6 +93,7 @@ var dp = (function () {
 
                 ani = new ANI(obj);
 
+                // set initial state
                 load[obj.key].ini.call(ani);
 
                 this.stack.push(ani);
