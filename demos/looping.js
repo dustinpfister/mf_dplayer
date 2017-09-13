67da385abc64865c6b33c308f2ba291246a33c54
dustinpfister <dustin.pfister@gmail.com>
2017-09-13 13:17:32 -0400

    nice looping demo

--- demos/looping.js
+++ demos/looping.js
@@ -12,10 +12,10 @@ dp.def({
             x : 0,
             y : 0,
             a : 0,
-            w : 16,
-            h : 16,
-            hw : 8,
-            hh : 8,
+            w : 8,
+            h : 8,
+            hw : 4,
+            hh : 4,
             s : '#ffffff',
             f : '#ff0000',
             i : 3,
@@ -35,9 +35,11 @@ dp.def({
 
         s.bx.forEach(function (bx, i) {
 
-            bx.he = _.tau * s.p
-                bx.x = s.unit.x + Math.cos(bx.he) * 100 * s.b;
-            bx.y = s.unit.y + Math.sin(bx.he) * 100 * s.b;
+            bx.he = _.tau * s.p;
+            bx.x = s.unit.x + s.unit.hw - bx.hw + Math.cos(bx.he) * bx.w;
+            bx.y = s.unit.y + s.unit.hh - bx.hh + Math.sin(bx.he) * bx.h;
+            //bx.x = s.unit.x + Math.cos(bx.he) * s.unit.w * s.b + bx.hw;
+            //bx.y = s.unit.y + Math.sin(bx.he) * s.unit.h * s.b + bx.hh;
 
         });
 
@@ -49,7 +51,9 @@ var unit = {
     x : 160 - 16,
     y : 120 - 16,
     w : 32,
-    h : 32
+    h : 32,
+    hw : 16,
+    hh : 16
 
 };
 
@@ -69,6 +73,10 @@ var loop = function () {
     requestAnimationFrame(loop);
 
     C.cls();
+
+    // draw unit area
+    C.ctx.fillStyle = '#808080';
+    C.ctx.fillRect(unit.x, unit.y, unit.w, unit.h);
 
     var i = s;
     while (i--) {
